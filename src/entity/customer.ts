41ac8266df0535880f1a3b419247014597a76ef1
Everton Zoelner <everton.zoelner@gmail.com>
2024-06-28 19:30:07 -0300

    feat: adiciona agregados

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,36 +1,72 @@
-class Customer {
-    private _id: string;
-    private _name: string;
-    private _address: string;
+import Address from "./address";
 
-    constructor(id: string, name: string, address: string) {
-        this._id = id;
-        this._name = name;
-        this._address = address;
-    }
+export default class Customer {
+  private _id: string;
+  private _name: string = "";
+  private _address!: Address;
+  private _active: boolean = false;
+  private _rewardPoints: number = 0;
 
-    get id() {
-        return this._id;
-    }
+  constructor(id: string, name: string) {
+    this._id = id;
+    this._name = name;
+    this.validate();
+  }
 
-    get name() {
-        return this._name;
-    }
+  get id(): string {
+    return this._id;
+  }
 
-    get address() {
-        return this._address;
-    }
+  get name(): string {
+    return this._name;
+  }
 
-    set id(id: string) {
-        this._id = id;
-    }
+  get rewardPoints(): number {
+    return this._rewardPoints;
+  }
 
-    set name(name: string) {
-        this._name = name;
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
     }
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+  }
 
-    set address(address: string) {
-        this._address = address;
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  get Address(): Address {
+    return this._address;
+  }
+  
+  changeAddress(address: Address) {
+    this._address = address;
+  }
+
+  isActive(): boolean {
+    return this._active;
+  }
+
+  activate() {
+    if (this._address === undefined) {
+      throw new Error("Address is mandatory to activate a customer");
     }
-    
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+
+  addRewardPoints(points: number) {
+    this._rewardPoints += points;
+  }
+
+  set Address(address: Address) {
+    this._address = address;
+  }
 }
